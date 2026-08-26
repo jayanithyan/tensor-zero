650446b26f8c1db7175f899cc9b0bffa1afe0b9e
jayanithyan <srisarankamalyarns83@gmail.com>
2026-08-26 17:21:20 +0530

    tensor attributes

--- 00_Blitz/Blitz_notes.py
+++ 00_Blitz/Blitz_notes.py
@@ -16,3 +16,8 @@ zeros_tensor = torch.zeros(shape)
 print(f"Random Tensor: \n {rand_tensor} \n")
 print(f"Ones Tensor: \n {ones_tensor} \n")
 print(f"Zeros Tensor: \n {zeros_tensor}")
+
+tensor = torch.rand(3, 4)
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
