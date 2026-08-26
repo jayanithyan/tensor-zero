6753e54906b6691d976452a1552ae84081a73ca2
jayanithyan <srisarankamalyarns83@gmail.com>
2026-08-26 17:17:13 +0530

    torch and tensor intialized

---
+++ 00_Blitz/Blitz_notes.py
@@ -0,0 +1,10 @@
+import numpy as np
+import torch
+data = [[1, 2], [3, 4]]
+x_data = torch.tensor(data)
+np_array = np.array(data)
+x_np = torch.from_numpy(np_array)
+x_ones = torch.ones_like(x_data) # retains the properties of x_data
+print(f"Ones Tensor: \n {x_ones} \n")
+x_rand = torch.rand_like(x_data, dtype=torch.float) # overrides the datatype of x_data
+print(f"Random Tensor: \n {x_rand} \n")
